843fa8999213b18c5cc0d701fc95fc43a0c76ddc
Max Stevenson <maxstevenson@msn.com>
2019-05-17 12:56:40 +0100

    Commit twenty-three. Developed concrete class implementing NodeList interface. NodeList interface created.

---
+++ src/main/java/AbstractClassChallenge/CustomLinkedList.java
@@ -0,0 +1,62 @@
+package AbstractClassChallenge;
+
+public class CustomLinkedList implements NodeList {
+
+    private ListItem root = null;
+
+    public CustomLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem item) {
+        if (this.root == null) {
+            this.root = item;
+        }
+
+        ListItem currentItem = this.root;
+        while (currentItem != null) {
+            int comparison = (currentItem.compareTo(item));
+            if (comparison < 0) {
+                if (currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    currentItem.setNext(item);
+                    item.setPrevious(currentItem);
+                    return true;
+                }
+            } else if (comparison > 0) {
+                if (currentItem.previous() != null) {
+                    currentItem.previous().setNext(item);
+                    item.setPrevious(currentItem.previous());
+                    item.setNext(currentItem);
+                    currentItem.setPrevious(item);
+                } else {
+                    item.setNext(this.root);
+                    this.root.setPrevious(item);
+                    this.root = item;
+                }
+                return true;
+            } else {
+                System.out.println(item.getValue() + " is already present - not added.");
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+
+    }
+}
