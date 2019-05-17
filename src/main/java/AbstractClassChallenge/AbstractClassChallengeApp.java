7a82807659f57551503e44e30cd70fdf08a54dd0
Max Stevenson <maxstevenson@msn.com>
2019-05-17 14:21:57 +0100

    Commit twenty-five. SearchTree class developed.

--- src/main/java/AbstractClassChallenge/AbstractClassChallengeApp.java
+++ src/main/java/AbstractClassChallenge/AbstractClassChallengeApp.java
@@ -2,17 +2,14 @@ package AbstractClassChallenge;
 
 public class AbstractClassChallengeApp {
     public static void main(String[] args) {
-        CustomLinkedList linkedList = new CustomLinkedList(null);
-//        linkedList.traverse(linkedList.getRoot());
+        SearchTree tree = new SearchTree(null);
 
         String stringData = "Space Soul Power Time Mind Reality";
         String[] data = stringData.split(" ");
         for (String s : data) {
-            linkedList.addItem(new Node(s));
+            tree.addItem(new Node(s));
         }
 
-        linkedList.traverse(linkedList.getRoot());
-        linkedList.removeItem(new Node("Time"));
-        linkedList.traverse(linkedList.getRoot());
+       tree.traverse(tree.getRoot());
     }
 }
