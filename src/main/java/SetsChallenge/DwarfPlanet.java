2783486cca9e2ccfaceab973841c0c879cfcd893
Max Stevenson <maxstevenson@msn.com>
2019-05-31 11:07:49 +0100

    Commit thirty-one. Created Moon subclass. Refactored code in ChallengeApp to utilise subclasses.

--- src/main/java/SetsChallenge/DwarfPlanet.java
+++ src/main/java/SetsChallenge/DwarfPlanet.java
@@ -4,4 +4,5 @@ public class DwarfPlanet extends HeavenlyBody {
     public DwarfPlanet(String name, double orbitalPeriod) {
         super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
     }
+
 }
---
+++ src/main/java/SetsChallenge/Moon.java
@@ -0,0 +1,7 @@
+package SetsChallenge;
+
+public class Moon extends HeavenlyBody {
+    public Moon(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.MOON);
+    }
+}
