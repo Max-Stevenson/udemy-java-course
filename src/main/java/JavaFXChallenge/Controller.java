8d25ececfa848532ef6314365515180c29cdca00
Max Stevenson <maxstevenson@msn.com>
2019-06-12 20:15:38 +0100

    Commit thirty-eight. TodoData class created for basic data persistence. Removed hardcoded values from Controller. App startup and shutdown now respectively loads and saves todo items.

--- src/main/java/JavaFXChallenge/Controller.java
+++ src/main/java/JavaFXChallenge/Controller.java
@@ -1,5 +1,6 @@
 package JavaFXChallenge;
 
+import JavaFXChallenge.model.TodoData;
 import JavaFXChallenge.model.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -8,10 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
 
 public class Controller {
 
@@ -22,44 +20,20 @@ public class Controller {
     @FXML
     private ListView<TodoItem> todoListView;
 
-    private List<TodoItem> todoItems;
-
     public void initialize() {
-        TodoItem item1 = new TodoItem("Birthday Card",
-                "Buy a 40th birthday card for your friend",
-                LocalDate.of(2019, 9, 14));
-        TodoItem item2 = new TodoItem("Doctor Appointment",
-                "Checkup appointment, blood tests",
-                LocalDate.of(2019, 6, 12));
-        TodoItem item3 = new TodoItem("Finish design prototype",
-                "Email mockups to boss",
-                LocalDate.of(2019, 12, 6));
-        TodoItem item4 = new TodoItem("Birthday Meal",
-                "Birthday meal with family",
-                LocalDate.of(2019, 6, 8));
-        TodoItem item5 = new TodoItem("Stock up on food",
-                "Get ready for the apocalypse",
-                LocalDate.of(2019, 6, 1));
-
-        todoItems = new ArrayList<>();
-        todoItems.add(item1);
-        todoItems.add(item2);
-        todoItems.add(item3);
-        todoItems.add(item4);
-        todoItems.add(item5);
-
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
             @Override
             public void changed(ObservableValue<? extends TodoItem> observable, TodoItem oldValue, TodoItem newValue) {
                 if (newValue != null) {
                     TodoItem item = todoListView.getSelectionModel().getSelectedItem();
                     todoItemDetails.setText(item.getDetails());
-                    deadlineLabel.setText(item.getDeadline().toString());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMM d, YYYY");
+                    deadlineLabel.setText(df.format(item.getDeadline()));
                 }
             }
         });
 
-        todoListView.getItems().setAll(todoItems);
+        todoListView.getItems().setAll(TodoData.getInstance().getTodoItemList());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
     }
---
+++ src/main/java/JavaFXChallenge/model/TodoData.java
@@ -0,0 +1,82 @@
+package JavaFXChallenge.model;
+
+import javafx.collections.FXCollections;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
+import java.util.List;
+
+public class TodoData {
+    private static TodoData instance = new TodoData();
+    private static String filename = "TodoListItems.txt";
+
+    private List<TodoItem> todoItemList;
+    private DateTimeFormatter dateTimeFormatter;
+
+    public static TodoData getInstance() {
+        return instance;
+    }
+
+    private TodoData() {
+        dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    }
+
+    public List<TodoItem> getTodoItemList() {
+        return todoItemList;
+    }
+
+    public void loadTodoItems() throws IOException {
+        todoItemList = FXCollections.observableArrayList();
+        Path path = Paths.get(filename);
+        BufferedReader bufferedReader = Files.newBufferedReader(path);
+        String input;
+
+        try {
+            while ((input = bufferedReader.readLine()) != null) {
+                String[] itemPieces = input.split("\t");
+
+                String shortDescription = itemPieces[0];
+                String details = itemPieces[1];
+                String dateString = itemPieces[2];
+
+                LocalDate date = LocalDate.parse(dateString, dateTimeFormatter);
+
+                TodoItem todoItem = new TodoItem(shortDescription, details, date);
+                todoItemList.add(todoItem);
+            }
+
+        } finally {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+        }
+    }
+
+    public void storeTodoItems() throws IOException {
+        Path path = Paths.get(filename);
+        BufferedWriter bufferedWriter = Files.newBufferedWriter(path);
+        try {
+            Iterator<TodoItem> iterator = todoItemList.iterator();
+            while (iterator.hasNext()) {
+                TodoItem item = iterator.next();
+                bufferedWriter.write(String.format("%s\t%s\t%s",
+                        item.getDescription(),
+                        item.getDetails(),
+                        item.getDeadline()));
+                bufferedWriter.newLine();
+            }
+        } finally {
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+        }
+    }
+}
