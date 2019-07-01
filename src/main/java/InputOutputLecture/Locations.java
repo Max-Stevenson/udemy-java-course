8a5aeda490bc275ec1752974345591c4996c8b67
Max Stevenson <maxstevenson@msn.com>
2019-07-01 20:03:55 +0100

    Commit forty-nine. Created InputOut Challenge files.

---
+++ src/main/java/InputOutputLecture/Locations.java
@@ -0,0 +1,134 @@
+package InputOutputLecture;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Locations implements Map<Integer, Location> {
+    private static Map<Integer, Location> locations = new HashMap<>();
+
+    public static void main(String[] args) {
+        FileWriter locFile = null;
+        try {
+            locFile = new FileWriter("locations.txt");
+            for (Location location : locations.values()) {
+                locFile.write(location.getLocationID() + ", " + location.getDescription() + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (locFile != null) {
+                    System.out.println("Closing file...");
+                    locFile.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static {
+        Map<String, Integer> tempExit = new HashMap<>();
+        locations.put(0, new Location(0, "You are sitting at a PC learning Java", tempExit));
+
+        tempExit = new HashMap<>();
+        tempExit.put("W", 2);
+        tempExit.put("N", 5);
+        tempExit.put("E", 3);
+        tempExit.put("S", 4);
+        locations.put(1, new Location(1, "You are standing at the end of the road before a small brick building", tempExit));
+
+        tempExit = new HashMap<>();
+        tempExit.put("N", 5);
+        locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
+
+        tempExit = new HashMap<>();
+        tempExit.put("W", 1);
+        locations.put(3, new Location(3, "You are inside a building, with dark stairs leading to the basement", tempExit));
+
+        tempExit = new HashMap<>();
+        tempExit.put("N", 1);
+        tempExit.put("W", 2);
+        locations.put(4, new Location(4, "You are in a shaded woodland next to a shallow brook", tempExit));
+
+        tempExit = new HashMap<>();
+        tempExit.put("S", 1);
+        tempExit.put("W", 2);
+        locations.put(5, new Location(5, "You are in a dank and musty cave", tempExit));
+
+        Map<String, String> commands = new HashMap<>();
+        commands.put("QUIT", "Q");
+        commands.put("NORTH", "N");
+        commands.put("EAST", "E");
+        commands.put("SOUTH", "S");
+        commands.put("WEST", "W");
+    }
+
+    public Location get(Integer key) {
+        return locations.get(key);
+    }
+
+    @Override
+    public int size() {
+        return locations.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return locations.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return locations.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return locations.containsValue(value);
+    }
+
+    @Override
+    public Location get(Object key) {
+        return locations.get(key);
+    }
+
+    @Override
+    public Location put(Integer key, Location value) {
+        return locations.put(key, value);
+    }
+
+    @Override
+    public Location remove(Object key) {
+        return locations.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends Location> m) {
+
+    }
+
+    @Override
+    public void clear() {
+        locations.clear();
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        return locations.keySet();
+    }
+
+    @Override
+    public Collection<Location> values() {
+        return locations.values();
+    }
+
+    @Override
+    public Set<Entry<Integer, Location>> entrySet() {
+        return locations.entrySet();
+    }
+}
