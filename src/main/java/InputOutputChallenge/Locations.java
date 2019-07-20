8b3425df012e44db66b1c3aaf7a134a42319b471
Max Stevenson <maxstevenson@msn.com>
2019-07-20 21:38:42 +0100

    Commit fifty-five. Refactored Locations to utilise try with resources.

--- src/main/java/InputOutputChallenge/Locations.java
+++ src/main/java/InputOutputChallenge/Locations.java
@@ -19,9 +19,7 @@ public class Locations implements Map<Integer, Location> {
     }
 
     static {
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(new FileReader("locations.txt"));
+        try(Scanner scanner = new Scanner(new FileReader("/Users/maxstevenson/IdeaProjects/udemy-java-course/src/main/java/InputOutputChallenge/locations_big.txt"))) {
             scanner.useDelimiter(",");
             while (scanner.hasNextLine()) {
                 int loc = scanner.nextInt();
@@ -33,14 +31,9 @@ public class Locations implements Map<Integer, Location> {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (scanner != null) {
-                scanner.close();
-            }
         }
 
-        try {
-            scanner = new Scanner(new BufferedReader(new FileReader("directions.txt")));
+        try(Scanner scanner = new Scanner(new BufferedReader(new FileReader("/Users/maxstevenson/IdeaProjects/udemy-java-course/src/main/java/InputOutputChallenge/directions_big.txt")))) {
             scanner.useDelimiter(",");
             while (scanner.hasNextLine()) {
                 int loc = scanner.nextInt();
@@ -55,45 +48,7 @@ public class Locations implements Map<Integer, Location> {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (scanner != null) {
-                scanner.close();
-            }
         }
-//        Map<String, Integer> tempExit = new HashMap<>();
-//        locations.put(0, new Location(0, "You are sitting at a PC learning Java", tempExit));
-//
-//        tempExit = new HashMap<>();
-//        tempExit.put("W", 2);
-//        tempExit.put("N", 5);
-//        tempExit.put("E", 3);
-//        tempExit.put("S", 4);
-//        locations.put(1, new Location(1, "You are standing at the end of the road before a small brick building", tempExit));
-//
-//        tempExit = new HashMap<>();
-//        tempExit.put("N", 5);
-//        locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
-//
-//        tempExit = new HashMap<>();
-//        tempExit.put("W", 1);
-//        locations.put(3, new Location(3, "You are inside a building, with dark stairs leading to the basement", tempExit));
-//
-//        tempExit = new HashMap<>();
-//        tempExit.put("N", 1);
-//        tempExit.put("W", 2);
-//        locations.put(4, new Location(4, "You are in a shaded woodland next to a shallow brook", tempExit));
-//
-//        tempExit = new HashMap<>();
-//        tempExit.put("S", 1);
-//        tempExit.put("W", 2);
-//        locations.put(5, new Location(5, "You are in a dank and musty cave", tempExit));
-//
-//        Map<String, String> commands = new HashMap<>();
-//        commands.put("QUIT", "Q");
-//        commands.put("NORTH", "N");
-//        commands.put("EAST", "E");
-//        commands.put("SOUTH", "S");
-//        commands.put("WEST", "W");
     }
 
     public Location get(Integer key) {
